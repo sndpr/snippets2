98544c27e4f1cb149a12105026527ea676eb5ad3
sykq <genericmailacc@gmail.com>
2019-07-16 22:11:39 +0200

    custom collector for grouping reduction

---
+++ src/main/java/org/psc/consec/ConsecutiveSequencesCalulator.java
@@ -0,0 +1,32 @@
+package org.psc.consec;
+
+import java.util.*;
+
+public class ConsecutiveSequencesCalulator {
+
+    public static List<List<Integer>> calcConsecutiveSequences(int sum) {
+        int upperBoundary = (int) Math.ceil(sum / 2.0);
+        int currentSum = 0;
+        int currentElem = 1;
+
+        List<List<Integer>> results = new ArrayList<>();
+        Deque<Integer> currentSequence = new LinkedList<>();
+
+        while (currentElem <= upperBoundary) {
+            if (currentSum < sum) {
+                currentSum += currentElem;
+                currentSequence.addLast(currentElem);
+                currentElem++;
+            }
+            if (currentSum > sum) {
+                currentSum -= currentSequence.removeFirst();
+            }
+            if (currentSum == sum) {
+                results.add(new LinkedList<>(currentSequence));
+                currentSum -= currentSequence.removeFirst();
+            }
+        } return results;
+    }
+
+
+}
---
+++ src/test/java/org/psc/consec/ConsecutiveSequencesCalulatorTest.java
@@ -0,0 +1,34 @@
+package org.psc.consec;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@Slf4j
+class ConsecutiveSequencesCalulatorTest {
+
+    @Test
+    void testCalcConsecutiveSequences() {
+        List<List<Integer>> consecutiveSequences = ConsecutiveSequencesCalulator.calcConsecutiveSequences(5);
+
+        consecutiveSequences.stream()
+                .map(e -> e.stream().map(String::valueOf).collect(Collectors.joining(", ")))
+                .forEach(log::info);
+
+        assertThat(consecutiveSequences.size(), is(1));
+
+        consecutiveSequences = ConsecutiveSequencesCalulator.calcConsecutiveSequences(100);
+
+        consecutiveSequences.stream()
+                .map(e -> e.stream().map(String::valueOf).collect(Collectors.joining(", ")))
+                .forEach(log::info);
+
+        assertThat(consecutiveSequences.size(), is(2));
+    }
+
+}
