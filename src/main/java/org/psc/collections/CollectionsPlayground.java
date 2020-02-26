bad8006a39fb24b1e9ec5e18f7f7f5d98cc4ff3e
sykq <genericmailacc@gmail.com>
2020-02-26 19:32:43 +0100

    commons-collections4

---
+++ src/main/java/org/psc/collections/CollectionsPlayground.java
@@ -0,0 +1,71 @@
+package org.psc.collections;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.BidiMap;
+import org.apache.commons.collections4.OrderedMap;
+import org.apache.commons.collections4.Trie;
+import org.apache.commons.collections4.bidimap.TreeBidiMap;
+import org.apache.commons.collections4.map.LinkedMap;
+import org.apache.commons.collections4.trie.PatriciaTrie;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+@Slf4j
+public class CollectionsPlayground {
+
+    public static OrderedMap<String, String> orderedMap() {
+        OrderedMap<String, String> orderedMap = new LinkedMap<>();
+
+        orderedMap.put("first", "abcac");
+        orderedMap.put("second", "56451");
+        orderedMap.put("third", "nothing");
+        orderedMap.put("fourth", "f");
+        orderedMap.put("fifth", "pld");
+
+        String secondValue = orderedMap.get(orderedMap.previousKey("third"));
+
+        log.info(secondValue);
+
+        return orderedMap;
+    }
+
+    public static BidiMap<String, Integer> bidiMapFromString(String input) {
+        return Stream.of(StringUtils.split(input, '\n'))
+                .map(CollectionsPlayground::fromSimpleMapping)
+                .collect(Collector.of(TreeBidiMap::new,
+                        (accumulator, current) -> accumulator.put(current.getKey(), current.getValue()),
+                        (a, b) -> {
+                            a.putAll(b);
+                            return a;
+                        }));
+    }
+
+    public static Trie<String, String> trie() {
+        Trie<String, String> trie = new PatriciaTrie<>();
+        trie.put("A", "1");
+        trie.put("ABC", "12");
+        trie.put("AAAA", "4412");
+        trie.put("ABABAB", "test");
+        trie.put("BYBE", "lok");
+        trie.put("AABC", "asdas");
+
+        Map<String, String> prefixMap = trie.prefixMap("AB");
+        prefixMap.forEach((key, value) -> log.info("{}: {}", key, value));
+        log.info("----");
+
+        Map<String, String> prefixMap2 = trie.prefixMap("AA");
+        prefixMap2.forEach((key, value) -> log.info("{}: {}", key, value));
+
+        return trie;
+    }
+
+    private static Map.Entry<String, Integer> fromSimpleMapping(String input) {
+        String[] splitInput = StringUtils.split(input, ':');
+        return new AbstractMap.SimpleEntry<>(splitInput[0], Integer.valueOf(splitInput[1]));
+    }
+
+}
---
+++ src/test/java/org/psc/collections/CollectionsPlaygroundTest.java
@@ -0,0 +1,62 @@
+package org.psc.collections;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.BidiMap;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class CollectionsPlaygroundTest {
+
+    @Test
+    void testOrderedMap() {
+        var orderedMap = CollectionsPlayground.orderedMap();
+        orderedMap.forEach((key, value) -> log.info("{}: {}", key, value));
+
+        assertThat(orderedMap).isNotEmpty();
+    }
+
+    @Test
+    void testBidiMapFromString() {
+        var bidiMapInput = """
+                a:1
+                B:2222
+                d:-454
+                x:213123
+                C:3424
+                H:77
+                """;
+
+        BidiMap<String, Integer> bidiMap = CollectionsPlayground.bidiMapFromString(bidiMapInput);
+
+        assertThat(bidiMap.get("C")).isEqualTo(3424);
+        assertThat(bidiMap.getKey(77)).isEqualTo("H");
+    }
+
+    @Test
+    void testBidiMapFromStringDuplicateValue() {
+        var bidiMapInput = """
+                a:1
+                B:2222
+                d:-454
+                x:213123
+                C:3424
+                H:77
+                F:1
+                """;
+
+        BidiMap<String, Integer> bidiMap = CollectionsPlayground.bidiMapFromString(bidiMapInput);
+
+        // a gets override by F
+        assertThat(bidiMap.get("a")).isNull();
+        assertThat(bidiMap.getKey(1)).isEqualTo("F");
+    }
+
+    @Test
+    void testTrie(){
+        var trie = CollectionsPlayground.trie();
+
+        assertThat(trie).isNotEmpty();
+    }
+}
