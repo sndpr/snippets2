f91a5e79c547ac877b97df3c363bdf1157972af6
sykq <genericmailacc@gmail.com>
2020-06-01 16:25:22 +0200

    resteasy

---
+++ src/main/java/org/psc/resteasy/ClientApplication.java
@@ -0,0 +1,29 @@
+package org.psc.resteasy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.client.WebTarget;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        ClientRequestFilter requestFilter =
+                requestContext -> requestContext.getHeaders().add("fromFilterKey", "fromFilterValue");
+        Client client = ResteasyClientBuilder.newClient().register(requestFilter);
+        WebTarget webTarget = client.target("http://localhost:8080/api/service/misc/echo?value=abc");
+
+        //noinspection unchecked
+        Map<String, Object> response = webTarget.request().header("testKey", "testValue").get(Map.class);
+
+        log.info(response.entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + " : " + entry.getValue())
+                .collect(Collectors.joining(", ")));
+    }
+}
