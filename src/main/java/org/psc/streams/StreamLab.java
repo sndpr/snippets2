09cfcd80da4c2373a1db108556941c6198493180
sykq <genericmailacc@gmail.com>
2021-03-19 19:04:13 +0100

    java 16

--- src/main/java/org/psc/streams/StreamLab.java
+++ src/main/java/org/psc/streams/StreamLab.java
@@ -1,22 +1,10 @@
 package org.psc.streams;
 
-import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class StreamLab {
-
-    public static void main(String[] args) {
-        Stream<Object> objects = Stream.of("test", "!", 1);
-        List<String> filterCastStrings = objects
-                .mapMulti(filterCast(String.class))
-                .toList();
-
-        List<String> filterCastStrings1 = objects
-                .flatMap(it -> filterCast(it, String.class))
-                .toList();
-}
 
     public static <I, O> Stream<O> filterCast(I inputElement, Class<O> outputType) {
         return outputType.isInstance(inputElement) ? Stream.of(outputType.cast(inputElement)) : Stream.empty();
--- src/test/java/org/psc/streams/StreamLabTest.java
+++ src/test/java/org/psc/streams/StreamLabTest.java
@@ -3,7 +3,6 @@ package org.psc.streams;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,11 +10,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StreamLabTest {
 
     @Test
-    void testFilterCast(){
+    void testMapMultiFilterCast() {
+        Stream<Object> objects = Stream.of("test", "!", 1);
+        List<String> filterCastStrings = objects
+                .mapMulti(StreamLab.filterCast(String.class))
+                .toList();
+
+        assertThat(filterCastStrings).containsExactly("test", "!");
+    }
+
+    @Test
+    void testMapMultiFilterCast2() {
+        Stream<Object> objects = Stream.of("test", "!", 1);
+
+        List<String> instanceOfPatternMatched = objects
+                .mapMulti((obj, downstream) -> {
+                    if (obj instanceof String s) {
+                        downstream.accept(s);
+                    }
+                })
+                .map(String.class::cast)
+                .toList();
+
+        assertThat(instanceOfPatternMatched).containsExactly("test", "!");
+    }
+
+    @Test
+    void testFilterCast() {
         Stream<Object> objects = Stream.of("test", 1);
         List<String> strings = objects
                 .flatMap(it -> StreamLab.filterCast(it, String.class))
-                .collect(Collectors.toList());
+                .toList();
 
         assertThat(strings).containsExactly("test");
 
